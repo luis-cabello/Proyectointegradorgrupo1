41eca67807b78e1231feeadc9a31a64efd390ce8
Luis Pedro Cabello <luispedrocabello@MacBook-Air-de-Luis-2.local>
2021-11-03 12:27:00 -0300

    arrancque con fetch y el index

---
+++ js/index.js
@@ -0,0 +1,26 @@
+//Lo mas visto en peliculas//
+let url = ("https://api.themoviedb.org/3/movie/top_rated?api_key=7a176cc95147be6e695be2faf0e8ff9c")
+
+fetch(url)
+.then(function(respuesta){
+   return respuesta.json()
+   
+})
+.then(function(datos){
+    for(let i=0; i<=4; i++)
+    console.log(datos.results[i])
+
+    document.querySelector(".atributoflex2").innerHTML+=`
+    
+<article>
+    <div class="foto">
+            <img src=" "  alt=" "> 
+    </div>
+    <h2>${datos.results[i].title}</h2>
+    <p>${datos.results[i].release_date}</p>
+</article>`
+   
+    
+{
+   
+}})
