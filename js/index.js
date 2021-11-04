75a288ee714a0ba06b0397c99b939477e9611a8b
Luis Pedro Cabello <luispedrocabello@MacBook-Air-de-Luis-2.local>
2021-11-04 16:34:38 -0300

    termine index.js faltan las imagenes, me toca hacer detalles

--- js/index.js
+++ js/index.js
@@ -2,16 +2,18 @@
 let url = ("https://api.themoviedb.org/3/movie/top_rated?api_key=7a176cc95147be6e695be2faf0e8ff9c")
 
 fetch(url)
-.then(function(respuesta){
-   return respuesta.json()
-   
-})
-.then(function(datos){
-    for(let i=0; i<=4; i++){
-    console.log(datos.results[i])
+    .then(function (respuesta) {
+        return respuesta.json();
 
-    document.querySelector(".atributoflex2").innerHTML +=`
-    
+    })
+    .then(function (datos) {
+        for (let i = 0; i <= 4; i++) {
+            console.log(datos.results[i]);
+
+            //Creo estructura y modifico HTML mediante innerHTML//
+
+            document.querySelector(".atributoflex2").innerHTML += `
+           
 <article>
     <div class="foto">
             <img src=" "  alt=" "> 
@@ -20,13 +22,71 @@ fetch(url)
     <p>${datos.results[i].release_date}</p>
 </article>
 `
-   
-    
-    }
 
-   
-})
+
+        }
+
+
+    })
 
 //Series Populares//
 
+let url2 = ("https://api.themoviedb.org/3/tv/popular?api_key=7a176cc95147be6e695be2faf0e8ff9c ")
 
+fetch(url2)
+
+    .then(function (respuesta) {
+        return respuesta.json()
+    })
+
+    .then(function (datos) {
+        for (let i = 0; i <= 4; i++) {
+            console.log(datos.results[i]);
+
+
+
+            //Creo esstrcutura y cambio HTML mediante innerHTML//
+            document.querySelector(".atributoflex").innerHTML += `
+
+<article> 
+
+    <div class="foto">
+            <img src=" "  alt=" ">
+    </div>
+    <h2>${datos.results[i].name}</h2>
+    <p>${datos.results[i].first_air_date}</p>
+</article> 
+`
+        }
+    })
+
+//Peliculas recomendadas//
+
+let url3 = ("https://api.themoviedb.org/3/movie/popular?api_key=7a176cc95147be6e695be2faf0e8ff9c")
+
+fetch(url3)
+
+    .then(function (respuesta) {
+        return respuesta.json()
+    })
+
+    .then(function (datos) {
+        for (let i = 0; i <= 4; i++) {
+            console.log(datos.results[i]);
+
+
+            //De nuevo creo estructura HTML pero con disintas propiedades//
+
+
+            document.querySelector(".atributoflex3").innerHTML += `
+<article> 
+
+    <div class="foto">
+            <img src=" "  alt=" ">
+    </div>
+    <h2>${datos.results[i].title}</h2>
+    <p>${datos.results[i].release_date}</p>
+</article> 
+`
+        }
+    })
