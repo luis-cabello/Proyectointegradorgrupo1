32160e4d760259563f9580087b2fd6ef25b087c1
pedromaina <Pmainardi@udesa.edu.ar>
2021-11-16 15:27:43 -0300

    results terminando

--- js/results.js
+++ js/results.js
@@ -3,24 +3,20 @@ let queryString = location.search;
 //la transformamos en Objeto literal
 let queryStringObj = new URLSearchParams(queryString);
 //name del campo input del formulario
-let formulario = queryStringObj.get("search");
+let formulario = queryStringObj.get("q");
 
 let datoBuscado = document.querySelector(".result-titulo");
 datoBuscado.innerHTML += `Resultados para ${formulario}`
 
-let url = ` https://api.themoviedb.org/3/search/multi?api_key=7a176cc95147be6e695be2faf0e8ff9c=${formulario}`;
+let url = ` https://api.themoviedb.org/3/search/multi?api_key=7a176cc95147be6e695be2faf0e8ff9c&query=${formulario}`;
 
 fetch(url)
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
-        let leyenda= document.querySelector(".leyenda");
-        if(data.data.length == 0){
-            leyenda.style.display = "block";
-        }
 
-        let peliculasData= data.data;
+        let peliculasData= data.results;
         let peliculas = document.querySelector(".Informacion-de-pelis");
         let infopeliculas ="";
         console.log(data)
@@ -28,12 +24,14 @@ fetch(url)
         for (let i=0; i<peliculasData.length; i++){
             infopeliculas +=
             `<artcile class="peliculas_home">
-            <a href="./detail-movie.html ?id=${peliculasData[i].peliculas.id}"><img src="${peliculasData[i].peliculas.poster_path}" alt="peliculas imagen"></a>
-            <h4><a href="./detail-movie.html ?id=${peliculasData[i].peliculas.id}">Pelicula:${peliculasData[i].peliculas.name}</a></h4>
+            <a href="./detail-movie.html ?id=${peliculasData[i].id}"><img src="${peliculasData[i].peliculas.poster_path}" alt="peliculas imagen"></a>
+            <h4><a href="./detail-movie.html ?id=${peliculasData[i].id}">Pelicula:${peliculasData[i].peliculas.name}</a></h4>
             </artcile>
             <artcile class="peliculas_home">
             <a href="./detail-movie.html ?id=${peliculasData[i].peliculas.id}"><img src="${peliculasData[i].datos.poster_path}" alt="series imagen"></a>
             `
             peliculas.innerHTML += infopeliculas
+        
         }
+     
     })
