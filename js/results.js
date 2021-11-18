8fdba9a0d1f7091098896de81e4314f7f529cc5a
pedromaina <Pmainardi@udesa.edu.ar>
2021-11-18 10:30:51 -0300

    Cambio en el forms, se agrego un fetch para mostrar las series, trate de hacer que los resultados null no aparescan pero no me fue posible

--- js/results.js
+++ js/results.js
@@ -22,7 +22,6 @@ fetch(url)
 
         for (let i=0; i<peliculasData.length; i++){
             peliculas.innerHTML += 
-            
     
             `<article>
             <a class= "letrablanca" href="./detail-movie.html?id=${peliculasData[i].id}"><img src="https://image.tmdb.org/t/p/w342/${peliculasData[i].poster_path}" alt="peliculas imagen"></a>
@@ -32,23 +31,60 @@ fetch(url)
            console.log(peliculasData[i])
         
         }
-     
-        //Validar Formularios// 
-        let formulario = document.querySelector('form');
-        let inputfield = document.querySelector('.search');
-       
-        
+    
 
-        formulario.addEventListener('submit', function (evento) {
-            evento.preventDefault();
-            console.log("no se envio")
+let urlseries=`https://api.themoviedb.org/3/search/tv?api_key=7a176cc95147be6e695be2faf0e8ff9c&query=${formulario}`;
 
-            if (inputfield.value =="") {
-                confirm("no ha ingresado ningun termino, desea continuar?")
-            } else if (inputfield.value.length < 3) {
-                alert("Ingrese al menos 3 terminos")
-            } else {
-                this.submit();
-            }
-        })
+fetch(urlseries)
+.then(function (response) {
+    return response.json();
+})
+.then(function(data){
+  let seriesData= data.results;
+  let series= document.querySelector(".Series-results")
+  console.log(data)
+
+  for(let i=0; i<seriesData.length; i++){
+      series.innerHTML +=
+      `<article>
+      <a class= "letrablanca" href="./detail-series.html?id=${seriesData[i].id}"><img src="https://image.tmdb.org/t/p/w342/${seriesData[i].poster_path}" alt="series imagen"></a>
+      <h4><a class= "letrablanca" href="./detail-series.html?id=${seriesData[i].id}">Serie:${seriesData[i].name}</a></h4>
+      </article>`
+      console.log(seriesData[i])
+  }
+})
+
+   
+
+    
+    })
+    //Validar Formularios// 
+    let form = document.querySelector('form');
+    let inputfield = document.querySelector('.search');
+
+    form.addEventListener('submit', function (evento) {
+        evento.preventDefault();
+        console.log("no se envio")
+
+        if (inputfield.value =="") {
+            confirm("no ha ingresado ningun termino, desea continuar?")
+        } else if (inputfield.value.length < 3) {
+            alert("Ingrese al menos 3 terminos")
+        } else {
+            this.submit();
+        }
+    })  
+    //Quiero que no aparescan resultados para null// 
+
+    form.addEventListener('submit',function(evento){
+        evento.preventDefault();
+        console.log("No hay resultados")
+
+        if (peliculas.name == null ){
+           alert("No se encuentran resultados para su busqueda, desea continuar?")
+        }else {
+           let Finding = document.querySelector("No-results")
+           Finding.innerText += 
+           `<h2 class="no-results">"No se encuentran resultados para su busqueda"</h2>`
+        }
     })
